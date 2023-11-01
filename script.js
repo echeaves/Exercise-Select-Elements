b703b188632d5c0fa2a0383017ca4d33548016ca
echeaves <139502640+echeaves@users.noreply.github.com>
2023-11-01 12:53:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let heading = document.querySelector("h1");
+
+console.log(heading);
+
+let instructions = document.querySelector('ul');
+console.log(instructions);
+
+let instructionList = document.getElementById('instructionList');
+console.log(instructionList);
